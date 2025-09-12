d21bbf41d8f7278a6d8f7287b704431162074f01
Khrysler Jimenez <khryslerjimenez171@gmail.com>
2025-09-12 12:52:13 -0500

    Add files via upload

---
+++ PS4P4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //define variables
+    string name;
+    float cost, warranty, total;
+    
+    // input phase
+    cout << "Enter Appliance Name: ";
+    cin >> name;
+    cout << "Enter Appliance Cost: ";
+    cin >> cost;
+    
+    if (cost > 1000.00)
+    {
+        warranty = 0.10 * cost;
+    }
+    else
+    {
+        warranty = 0.05 * cost;
+    }
+    
+    total = cost + warranty;
+    
+    // process phase
+    cout << "Appliance: " << name << endl;
+    cout << "Cost: $" << cost << endl;
+    cout << "Warranty Cost: $" << warranty << endl;
+    cout << "Total: $" << total << endl;
+    
+    
+    return 0;
+    
+} // end of main
+
+
+
+
