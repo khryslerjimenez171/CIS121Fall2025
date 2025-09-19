b10094932b8043f29eb288d9672fc5b7bcdf73e5
Khrysler Jimenez <khryslerjimenez171@gmail.com>
2025-09-18 22:47:58 -0500

    Add files via upload

---
+++ PS5P2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //define variables
+    float lbs, priceperpound, total;
+
+    // input phase
+    cout << "Enter Quantity of Apples (lbs): ";
+    cin >> lbs;
+
+    if (lbs > 100)
+    {
+        priceperpound = 0.10;
+    }
+    else if (lbs >= 50)
+    {
+        priceperpound = 0.25;
+    }
+    else
+    {
+        priceperpound = 0.50;
+        return 1;
+    }
+
+    total = lbs * priceperpound;
+    
+    // process phase
+    cout << "Price Per Pound: $" << priceperpound << endl;
+    cout << "Total Cost: $" << total << endl;
+
+    return 0;
+
+} // end of main
