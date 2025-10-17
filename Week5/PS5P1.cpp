dbf6d36db7726c2217ca99e7eff2cf989b36d598
Khrysler Jimenez <khryslerjimenez171@gmail.com>
2025-10-16 20:58:56 -0500

    Add files via upload

---
+++ Week5/PS5P1.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    //define variables
+//    string lastname;
+//    int score;
+//    char grade;
+//
+//    // input phase
+//    cout << "Enter Student's Last Name: ";
+//    cin >> lastname;
+//    cout << "Enter Student's Score: ";
+//    cin >> score;
+//
+//    if (score >= 90)
+//    {
+//        grade = 'A';
+//    }
+//    else if (score >= 80)
+//    {
+//        grade = 'B';
+//    }
+//    else if (score >= 70)
+//    {
+//        grade = 'C';
+//    }
+//    else if (score >= 60)
+//    {
+//        grade = 'D';
+//    }
+//    else
+//    {
+//        grade = 'F';
+//        return 1;
+//    }
+//
+//    // process phase
+//    cout << "Student: " << lastname << endl;
+//    cout << "Grade: " << grade << endl;
+//
+//    return 0;
+//
+//} // end of main
---
+++ Week5/PS5P4.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    //define variables
+//    float annualsalary, taxrate, taxamount;
+//
+//    // input phase
+//    cout << "Enter Annual Salary: $";
+//    cin >> annualsalary;
+//
+//    if (annualsalary > 100000)
+//    {
+//        taxrate = 0.40;
+//    }
+//    else if (annualsalary >= 50000)
+//    {
+//        taxrate = 0.35;
+//    }
+//    else
+//    {
+//        taxrate = 0.25;
+//        return 1;
+//    }
+//
+//    taxamount = annualsalary * taxrate;
+//    
+//    // process phase
+//    cout << "Annual Salary: $" << annualsalary << endl;
+//    cout << "Tax Rate: " << (taxrate * 100) << "%" << endl;
+//    cout << "Tax Amount: $" << taxamount << endl;
+//
+//    return 0;
+//
+//} // end of main
