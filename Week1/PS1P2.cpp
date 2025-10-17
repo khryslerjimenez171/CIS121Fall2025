373d9d4ca71f77339cd55c25915ae5d3a8874fad
Khrysler Jimenez <khryslerjimenez171@gmail.com>
2025-10-16 20:47:27 -0500

    Add files via upload

---
+++ Week1/PS1P2.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    // define variables
+//    int n1, n2, sum;
+//    
+//    // input phase
+//    cout << "Enter two real numbers: ";
+//    cin >> n1 >> n2;
+//    
+//    sum = n1 + n2;
+//    
+//    // process phase
+//    cout << "The sum is: " << sum << endl;
+//    
+//    return 0;
+//    
+//} // end of main
---
+++ Week1/PS1P4.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//    // define variables
+//    int n1, n2, n3, result;
+//    
+//    // input phase
+//    cout << "Enter 3 integers: ";
+//    cin >> n1 >> n2 >> n3;
+//    
+//    result = (n1 + n2) * n3;
+//    
+//    // process phase
+//    cout << "The result is: " << result << endl;
+//    
+//    return 0;
+//    
+//} // end of main
