9f1afcf406ce0ef2a9296bba06e5640c2a0c9b15
Khrysler Jimenez <khryslerjimenez171@gmail.com>
2025-10-16 21:29:52 -0500

    Add files via upload

---
+++ Week 2/PS2P5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    // define variables
+    double price, discountpercent, discountamount, discountedprice;
+    
+    // input phase
+    cout << "Enter Price of Item: ";
+    cin >> price;
+    cout << "Enter Discount Percent (In Decimal Point): ";
+    cin >> discountpercent;
+    
+    discountamount = price * discountpercent;
+    discountedprice = price - discountamount;
+    
+    //process phase
+    cout << "Discount Amount: $" << discountamount << endl;
+    cout << "Discounted Price: $" << discountedprice << endl;
+    
+    return 0;
+    
+} // end of main
